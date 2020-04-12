3ef474ac190fa3fe36954cbccea6c311351320f0
stewartmuhanuzi <muhanuzistewart@gmail.com>
2020-04-12 22:29:08 +0300

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel=""
+        <link rel="stylesheet" href="*/css/main.css"
 
 
     </head>
